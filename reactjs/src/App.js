3341e07cc05be7a7fac9bab971263d7a27b6269a
moonhee <zipsy327@gmail.com>
2023-05-02 10:25:08 +0900

    reacttest

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -8,33 +8,33 @@ function App() {
     const [message, setMessage] = useState([]);
     const [data,setData]=useState([]);
     useEffect(() => {
-        fetch("/hello")
-            .then((response) => {
-                return response.json();
-            })
-            .then(function (data) {
-                setMessage(data);
-            });
+        // fetch("/hello")
+        //     .then((response) => {
+        //         return response.json();
+        //     })
+        //     .then(function (data) {
+        //         setMessage(data);
+        //     });
 
-        axios.get("/list")
-        .then(res=>{
-            setData(res.data);
-            console.log(res.data.length);
-        })
+        // axios.get("/list")
+        // .then(res=>{
+        //     setData(res.data);
+        //     console.log(res.data.length);
+        // })
     }, []);
 
     return (
         <div className="App">
             <header className="App-header">  
                 <img src={img1} style={{width:'200px'}}/>             
-                <ul>
+                {/* <ul>
                     {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-                </ul>
+                </ul> */}
             </header>
             <hr/>
             <section>
                 <h2>SpringBoot /list 로부터 받은 데이타 출력</h2>
-                {data.map((obj, index) => <li key={`${index}`}>{obj.name}:{obj.addr} 거주({obj.age}세)</li>)}
+                {/* {data.map((obj, index) => <li key={`${index}`}>{obj.name}:{obj.addr} 거주({obj.age}세)</li>)} */}
             </section>
         </div>
     );
